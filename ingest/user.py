6a1191e08e17aaa18b9f8e6d67a46e2ba43cb0b9
Data Salaryman <datasalaryman@protonmail.com>
2021-08-09 16:26:10 +0800

    Draft user pipeline

--- ingest/user.py
+++ ingest/user.py
@@ -1,4 +1,32 @@
 from creds import *
 import requests
 
-user_url = 'https://api.twitter.com/1.1/users/lookup.json'
+from dagster import pipeline, solid
+
+with open('.secrets/users-to-follow.csv') as f:
+    users = [line.rstrip() for line in f]
+
+headers = {}
+headers["Authorization"] = f'Bearer {bearer_token()}'
+
+@solid
+def user_ingest(context):
+    user_data = []
+    for user in users:
+        user_data = user_data + [
+            requests.get(
+                f'https://api.twitter.com/2/users/{user}', 
+                headers=headers
+            ).text
+        ]
+    context.log.info(f"Found {len(user_data)} users")
+    return user_data
+
+@pipeline
+def user_pipeline():
+    user_ingest()
+
+from dagster import execute_pipeline
+
+if __name__ == "__main__":
+    result = execute_pipeline(user_pipeline)
