1363e930357b0ae936f1828455dfac433c885ac9
Data Salaryman <datasalaryman@protonmail.com>
2021-08-09 15:45:34 +0800

    Add tweet and user URLs

---
+++ ingest/tweet.py
@@ -0,0 +1,4 @@
+from creds import *
+import requests
+
+tweet_url = 'https://api.twitter.com/1.1/users/lookup.json'
---
+++ ingest/user.py
@@ -0,0 +1,4 @@
+from creds import *
+import requests
+
+user_url = 'https://api.twitter.com/1.1/users/lookup.json'
